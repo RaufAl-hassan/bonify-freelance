db5011390d032ed027e516a812812962e5a8a5a7
RaufAl-hassan <alhassanr017@gmail.com>
2020-09-20 08:05:07 +0000

    updated navbar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 // UI variables
 const togglerIcon = document.querySelector(".navbar-toggler-icon");
-const navbar = document.querySelector(".navbar-left-right-links-wrapper");
+const mainNavbarLinksWrapper = document.querySelector(
+  ".navbar-left-right-links-wrapper"
+);
+const mainNavbar = document.querySelector(".main-navbar");
+const mainHeader = document.querySelector(".main-header");
 
 // handle navbar toggle
-togglerIcon.addEventListener("click", () => {
-  console.log(123);
-  navbar.classList.toggle("show-navbar-links");
-});
+togglerIcon.addEventListener("click", () =>
+  mainNavbarLinksWrapper.classList.toggle("show-navbar-links")
+);
+
+// navbar intersection Observer
+const mainNavbarSectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        mainNavbar.classList.add("main-navbar-section-observer");
+      } else {
+        mainNavbar.classList.remove("main-navbar-section-observer");
+      }
+    });
+  },
+  {
+    rootMargin: "-100px 0px 0px 0px",
+  }
+);
+
+mainNavbarSectionObserver.observe(mainHeader);
